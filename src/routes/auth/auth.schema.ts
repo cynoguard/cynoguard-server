2a48631b90821931fa9fa161075170531f55126a
Kaveesh <kaveeshwijetunge@gmail.com>
2026-01-26 19:31:55 +0530

    Update auth.schema.ts

--- src/routes/auth/auth.schema.ts
+++ src/routes/auth/auth.schema.ts
@@ -1,6 +1,7 @@
 import { Type, type Static } from "@sinclair/typebox";
 
 
+
 // Body Schema
 export const RegisterBodySchema = Type.Object({
   email: Type.String({ format: "email" }),
@@ -60,3 +61,29 @@ export const loginSchema = {
     }),
   }
 };  
+
+// OAuth Login Body
+export const SocialLoginSchema = Type.Object({
+  token: Type.String(),   
+});
+
+export type SocialLoginType = Static<typeof SocialLoginSchema>;
+
+// Full OAuth Login Schema for Swagger
+export const socialLoginSchema = {
+  summary: 'Social Login',
+  description: 'Login or Register automatically using a Google/Github Firebase token',
+  tags: ['Authentication'],
+  body:SocialLoginSchema,
+  response:{
+    200:Type.Object({
+      uid: Type.String(),
+      email: Type.String(),
+      role: Type.String(),
+      message: Type.String(),
+    }),
+    401:Type.Object({
+        error:Type.String(),
+    }),
+  }
+};
