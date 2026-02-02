ebc1c84da289a9d6307b2ce2cc6bf13540f77017
Mahima-Sanketh-Git <mahimadevops@gmail.com>
2026-02-02 15:16:52 +0530

    implement auth flow api routes

---
+++ src/routes/auth/auth.handler.ts
@@ -0,0 +1,38 @@
+import type { FastifyReply, FastifyRequest } from "fastify";
+import { verifyFirebaseToken } from "../../services/firebase.service.js";
+import { handleDbUser } from "../../services/user.service.js";
+
+
+export const authenticateUser = async (request: FastifyRequest, reply: FastifyReply) => {
+  const token = request.headers.authorization?.replace("Bearer ", "");
+  const body = request.body as { firstName?: string; lastName?: string };
+
+  try {
+    // 1. Verify Token (Source of Truth for UID and Email)
+    const decodedToken = await verifyFirebaseToken(token!);
+    if (!decodedToken) {
+      return reply.code(401).send({ error: "Unauthorized", message: "Invalid Firebase token" });
+    }
+
+    const { uid, email } = decodedToken;
+
+     const user = await handleDbUser(uid, email||"", body.firstName || "", body.lastName || "");
+
+    return reply.code(200).send({
+      status: "success",
+      message: "Authentication successful",
+      data: {
+        uid: user.firebaseId,
+        email: user.email,
+        role: user.role,
+        first_name: user.firstName || "",
+        last_name: user.lastName || "",
+        is_onboarded: user.isOnboarded,
+      }
+    });
+
+  } catch (error: any) {
+    console.error("Auth Error:", error);
+    return reply.code(500).send({ error: "Internal Server Error", message: error.message });
+  }
+};
--- src/services/firebase.service.ts
+++ src/services/firebase.service.ts
@@ -4,3 +4,11 @@ import { auth } from "../lib/firebase.js";
 export const createFirebaseAccount = async (email:string, password:string) =>{ 
     return await auth.createUser({email:email, password:password});
 };
+
+export const verifyFirebaseToken = async (token:string)=>{
+    try {
+        return await auth.verifyIdToken(token);
+    } catch (error) {
+        return null
+    }
+}
