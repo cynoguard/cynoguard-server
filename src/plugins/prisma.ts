2e0756d96165629adc24c7ab6c0e8c7a6938df0b
Mahima-Sanketh-Git <mahimadevops@gmail.com>
2026-03-15 21:13:03 +0530

    fixed:prisma local db url issue in prod

--- src/plugins/prisma.ts
+++ src/plugins/prisma.ts
@@ -1,10 +1,10 @@
 // src/lib/prisma.ts
-import "dotenv/config";
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@prisma/client';
+import "dotenv/config";
 
 const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL!,
+  connectionString: process.env.DATABASE_PROD_URL!,
 });
 
 export const prisma = new PrismaClient({adapter});
