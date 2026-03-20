d6a7c3ba7e2890deaab5c2a5a100e7c3e10c7ada
Mahima-Sanketh-Git <mahimadevops@gmail.com>
2026-03-20 21:02:29 +0530

    up v9

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,16 +2,13 @@ import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
 import "dotenv/config";
 
-// src/plugins/prisma.ts
-import "dotenv/config";
-import { readFileSync } from 'fs';
 
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_PROD_URL!,
-  ssl: {
-    rejectUnauthorized: true,
-    ca: readFileSync('/home/ubuntu/rds-ca.pem').toString(),
-  }
+  // ssl: {
+  //   rejectUnauthorized: true,
+  //   ca: readFileSync('/home/ubuntu/rds-ca.pem').toString(),
+  // }
 });
 
 export const prisma = new PrismaClient({ adapter });
