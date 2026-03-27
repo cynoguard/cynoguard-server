c6670206cdd86edd888c6a67988024dbc17cf170
Mahima-Sanketh-Git <mahimadevops@gmail.com>
2026-03-27 09:55:36 +0530

    up 98

--- src/plugins/prisma.ts
+++ src/plugins/prisma.ts
@@ -1,7 +1,8 @@
 // src/plugins/prisma.ts
+import "dotenv/config";
+
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from '@prisma/client';
-import "dotenv/config";
 import { readFileSync } from 'node:fs';
 
 const adapter = new PrismaPg({
