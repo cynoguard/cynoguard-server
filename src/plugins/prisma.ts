e7edf962b2d42dc65f0ea366c885696e6442a59f
Mahima-Sanketh-Git <mahimadevops@gmail.com>
2026-03-28 10:18:22 +0530

    fix: import PrismaClient from .prisma/client for Prisma 7

--- src/plugins/prisma.ts
+++ src/plugins/prisma.ts
@@ -1,5 +1,5 @@
+import { PrismaClient } from ".prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
-import PrismaClient from "@prisma/client";
 import "dotenv/config";
 import { readFileSync } from "node:fs";
 import { Pool } from "pg";
