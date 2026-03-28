00790cf9f4e75118e659580345211242961f19b3
Mahima-Sanketh-Git <mahimadevops@gmail.com>
2026-03-28 18:37:28 +0530

    ufb

--- src/plugins/prisma.ts
+++ src/plugins/prisma.ts
@@ -5,7 +5,7 @@ import { readFileSync } from "node:fs";
 import { Pool } from "pg";
 
 const pool = new Pool({
-  connectionString: "postgresql://postgres:cynoguard2026proddatabase1@cynoguard-prod-rds.cp0q4gcs4hbj.eu-north-1.rds.amazonaws.com:5432/cynoguard-prod-rds?sslmode=require",
+  connectionString: "postgresql://postgres:cynoguard2026proddatabase1@cynoguard-prod-rds.cp0q4gcs4hbj.eu-north-1.rds.amazonaws.com:5432/cynoguard-prod-rds?sslmode=require&sslcert=/home/ubuntu/rds-ca.pem",
   ssl: {
     rejectUnauthorized: true,
     ca: readFileSync("/home/ubuntu/rds-ca.pem").toString(),
