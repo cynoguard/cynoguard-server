5ff569d96a02a9c1d8be007da06ae0dd31422360
Mahima-Sanketh-Git <mahimadevops@gmail.com>
2026-03-28 10:10:09 +0530

    fix: remove adapter from prisma.config.ts, keep in PrismaClient only

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,4 +1,3 @@
-import { Pool } from "pg";
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
@@ -6,16 +5,7 @@ export default defineConfig({
   migrations: {
     path: "prisma/migrations",
   },
-  adapter: async () => {
-    const { PrismaPg } = await import("@prisma/adapter-pg");
-    const connectionString = process.env.DATABASE_PROD_URL!;
-    const pool = new Pool({ 
-      connectionString,
-      ssl: {
-        rejectUnauthorized: true,
-        ca: require("fs").readFileSync("/home/ubuntu/rds-ca.pem").toString(),
-      }
-    });
-    return new PrismaPg(pool);
+  datasource: {
+    url: process.env.DATABASE_PROD_URL!,
   },
 });
