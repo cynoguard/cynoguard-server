491ba402885b2c384dd42fb0238223f2dc6d867a
Mahima-Sanketh-Git <mahimadevops@gmail.com>
2026-03-28 09:57:19 +0530

    fix: prisma 7 breaking changes — move url to prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,8 +1,21 @@
+import { Pool } from "pg";
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
+  },
+  adapter: async () => {
+    const { PrismaPg } = await import("@prisma/adapter-pg");
+    const connectionString = process.env.DATABASE_PROD_URL!;
+    const pool = new Pool({ 
+      connectionString,
+      ssl: {
+        rejectUnauthorized: true,
+        ca: require("fs").readFileSync("/home/ubuntu/rds-ca.pem").toString(),
+      }
+    });
+    return new PrismaPg(pool);
   },
 });
